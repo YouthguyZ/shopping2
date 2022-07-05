6cd7253f03c8073b996c474da71c554edbd8a21a
youngguyz <2281300996@qq.com>
2022-07-05 21:48:12 +0800

    feat:完成了 axios 封装、配置路径别名

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <div>
     app 组件
   </div>
+  <h1>大和之</h1>
 </template>
 
 <script setup lang="ts">
-
+// 测试接口
+import request from '@/utils/request';
+request.get('/home/category/head').then(res=>console.log(res))
 </script>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 导入 path 
+const path=require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  // 配置@路径别名，方便导入模块
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
